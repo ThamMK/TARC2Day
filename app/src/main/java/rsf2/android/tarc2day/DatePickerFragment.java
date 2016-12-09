e53d3a7f19c888009885a8ca0108db4052e84250
soonPhing <neosp-wa14@student.tarc.edu.my>
2016-12-09 20:13:32 +0800

    Merge branch 'SocietyBranch' of https://github.com/ThamMK/TARC2Day
    # Conflicts:
    #	app/src/main/java/rsf2/android/tarc2day/MainActivity.java
    #	app/src/main/res/values/strings.xml

---
+++ app/src/main/java/rsf2/android/tarc2day/DatePickerFragment.java
@@ -0,0 +1,30 @@
+package rsf2.android.tarc2day;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.app.DialogFragment;
+import android.widget.DatePicker;
+import android.app.DatePickerDialog;
+import java.util.Calendar;
+
+/**
+ * Created by MingKeat on 3/12/2016.
+ */
+
+public class DatePickerFragment extends DialogFragment{
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        //Use the current date as the default date in the date picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+
+        return new DatePickerDialog(getActivity(), (PromotionCreate) getActivity(), year, month, day);
+    }
+
+
+
+}
